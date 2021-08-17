62f507b52ec277a44931078f2ac776b9976c8ea3
Ilijan Militar <ilijan.militar@gmail.com>
2021-08-17 14:03:19 +0200

    getBagButtons

--- app.js
+++ app.js
@@ -46,13 +46,42 @@ class UI {
     });
     productsDOM.innerHTML = result;
   }
+
+  getBagButtons() {
+    const buttons = [...document.querySelectorAll('.bag-btn')];
+    buttons.forEach(button => {
+      let id = button.dataset.id;
+      let inCart = cart.find(item => item.id === id);
+      if (inCart) {
+        button.innerText = 'In Cart';
+        button.disabled = true;
+      } else {
+        button.addEventListener('click', event => {
+          event.target.innerText = 'In Cart';
+          event.target.disabled = true;
+        });
+      }
+    });
+  }
 }
 
-class Storage {}
+class Storage {
+  static saveProducts(products) {
+    localStorage.setItem('products', JSON.stringify(products));
+  }
+}
 
 document.addEventListener('DOMContentLoaded', () => {
   const ui = new UI();
   const products = new Products();
 
-  products.getProducts().then(products => ui.dispplayProducts(products));
+  products
+    .getProducts()
+    .then(products => {
+      ui.dispplayProducts(products);
+      Storage.saveProducts(products);
+    })
+    .then(() => {
+      ui.getBagButtons();
+    });
 });
